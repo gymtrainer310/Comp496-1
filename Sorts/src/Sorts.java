eb828fc947d5a4c26e6e2e70740cf898f3f4bc8f
Gabriel Sturtevant <gsturtevant87@gmail.com>
2017-02-19 15:00:37 -0800

    Completed sorts functionality

--- Sorts/src/Sorts.java
+++ Sorts/src/Sorts.java
@@ -2,27 +2,127 @@ import java.lang.Math;
 
 public class Sorts
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int number_size = 1000000;
-        int cardinality = 10000000;
-        int[] nums = new int[cardinality];
-        for (int i = 0; i < cardinality; i++)
-        {
-            nums[i] = (int)(Math.random() * number_size);
+        int cardinality = 1;
+        int repetitions = 5;
+        int sets = 7;
+        System.out.println("Merge Sort, average of " + repetitions + " runs: ");
+        System.out.println("-----------------------------------------------");
+        System.out.format("%-1s %7s ", "|", "n");
+        System.out.format("%-1s %15s ", "|", "C(n)");
+        System.out.format("%-1s %15s %-1s %n", "|", "T(n)", "|");
+        System.out.println("|---------------------------------------------|");
+        for (int i = 0; i < sets; i++) {
+
+            long count = 0;
+            long time = 0;
+
+            if(i == (sets - 1))
+            {
+                cardinality *= 5;
+            }else
+            {
+                cardinality *= 10;
+            }
+
+            for (int j = 0; j < repetitions; j++) {
+
+                long start;
+                long end;
+                int[] nums = new int[cardinality];
+
+                for (int k = 0; k < cardinality; k++) {
+                    nums[k] = (int)(Math.random() * number_size);
+                }
+                start = System.nanoTime();
+                count += mergesort(nums);
+                end = System.nanoTime();
+                time += end - start;
+                if(!isSorted(nums))
+                {
+                    System.err.println("Array not sorted!");
+                    System.err.println("N: " +cardinality);
+                    System.exit(1);
+                }
+            }
+            //Uncomment for comma delimited raw output
+            //System.out.println(cardinality+","+(count/repetitions)+","+(time/repetitions));
+
+            System.out.format("%-1s %7d ", "|", cardinality);
+            System.out.format("%-1s %15d ", "|", (count/repetitions));
+            System.out.format("%-1s %15d %-1s %n", "|", (time/repetitions), "|");
         }
-        mergesort(nums);
-        System.out.println("Sorted!");
-        System.out.print(isSorted(nums));
+        System.out.println("-----------------------------------------------");
+        cardinality = 1;
+        System.out.println("\n\nInsertion Sort, average of " + repetitions + " runs:");
+        System.out.println("-----------------------------------------------");
+        System.out.format("%-1s %7s ", "|", "n");
+        System.out.format("%-1s %15s ", "|", "C(n)");
+        System.out.format("%-1s %15s %-1s %n", "|", "T(n)", "|");
+        System.out.println("|---------------------------------------------|");
+        for (int i = 0; i < sets; i++) {
+
+            long count = 0;
+            long time = 0;
+
+            if(i == (sets - 2))
+            {
+                cardinality *= 2;
+            }else if(i == (sets - 1))
+            {
+                cardinality *= 1.5;
+            }else
+            {
+                cardinality *= 10;
+            }
+
+            for (int j = 0; j < repetitions; j++) {
+
+                long start;
+                long end;
+                int[] nums = new int[cardinality];
+
+                for (int k = 0; k < cardinality; k++) {
+                    nums[k] = (int)(Math.random() * number_size);
+                }
+                start = System.nanoTime();
+                count += insertionsort(nums);
+                end = System.nanoTime();
+                time += end - start;
+                if(!isSorted(nums))
+                {
+                    System.err.println("Array not sorted!");
+                    System.err.println("N: " +cardinality);
+                    System.exit(2);
+                }
+            }
+            //Uncomment for comma delimited raw output
+            //System.out.println(cardinality+","+(count/repetitions)+","+(time/repetitions));
+
+            System.out.format("%-1s %7d ", "|", cardinality);
+            System.out.format("%-1s %15d ", "|", (count/repetitions));
+            System.out.format("%-1s %15d %-1s %n", "|", (time/repetitions), "|");
+        }
+        System.out.println("-----------------------------------------------");
     }
     /*--------------Insertion Sort -----------------------*/
     public static long insertionsort( int[] a)
     {
-        return (long)0;
+        long count = 0;
+        int temp;
+        for (int i = 1; i < a.length; i++) {
+            for(int j = i ; j > 0 ; j--){
+                count++;
+                if(a[j] < a[j-1]){
+                    temp = a[j];
+                    a[j] = a[j-1];
+                    a[j-1] = temp;
+                }
+            }
+        }
+        return count;
     }
-
-
-
 /* --------------------Merge Sort --------------------*/
     //merges sorted slices a[i.. j] and a[j + 1 …  k] for 0<=  i <=j < k < a.length
 
@@ -32,8 +132,7 @@ public class Sorts
         int[] temp = new int[size];
         int temp_i = i;
         int temp_j = j+1;
-        int temp_k = k;
-        int max = a.length;
+        long comps = 0;
 
         for (int l = 0; l < size ; l++) {
             if(temp_i == j+1)
@@ -45,42 +144,40 @@ public class Sorts
             }else if(a[temp_i] <= a[temp_j])
             {
                 temp[l] = a[temp_i++];
+                comps++;
             }else
             {
                 temp[l] = a[temp_j++];
+                comps++;
             }
         }
         for (int l = 0; l < size; l++) {
             a[i++] = temp[l];
         }
-        return (long)1;
+        return comps;
     }
-
-
     //sorts  a[ i .. k]  for 0<=i <= k < a.length
     private  static  long mergesort(int[] a,  int i ,  int k)
     {
+        long comps = 0;
         if((k-i)<=0)
         {
-            return (long)0;
+            return comps;
         }else
         {
             int middle = (i + k) / 2;
-            mergesort(a, i, middle);
-            mergesort(a, middle + 1, k);
-            merge(a, i, middle, k);
+            comps += mergesort(a, i, middle);
+            comps += mergesort(a, middle + 1, k);
+            comps += merge(a, i, middle, k);
         }
-        return (long)1;
+        return comps;
     }
-
     //Sorts the array using mergesort
     public static long mergesort(int[] a )
     {
-        mergesort(a,0, a.length-1);
-        return (long)1;
+        long comps = mergesort(a,0, a.length-1);
+        return comps;
     }
-
-
     public static boolean isSorted( int[] a)
     {
         for (int i = 1; i < a.length; i++) {
@@ -89,5 +186,4 @@ public class Sorts
         }
         return true;
     }
-
 }
